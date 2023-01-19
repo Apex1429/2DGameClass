31865fb35a38f0809f9fa23ffae644c43bb58278
Apex1429 <apex1429@gmail.com>
2023-01-18 21:02:58 -0500

    First Commit
    Project Start, button click + 1

---
+++ Assets/IdleTutorialGame.cs
@@ -0,0 +1,25 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class IdleTutorialGame : MonoBehaviour
+{
+
+    public Text coinsText;
+    public double coins;
+
+    public void Start() 
+    {
+        coins = 0;    
+    }
+
+    public void Update() 
+    {
+        coinsText.text = "Coins: " + coins;
+    }
+
+    public void Click()
+    {
+        coins += 1;
+    }
+
+}
